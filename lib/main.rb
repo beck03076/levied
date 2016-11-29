44f4e0b9e7b04fb2b5f78feb4f933eaedd08f1d0
beck03076 <senthilkumar.hce@gmail.com>
2016-11-30 01:59:47 +0530

    added main file

---
+++ lib/main.rb
@@ -0,0 +1,29 @@
+class Main
+
+  def execute
+    files = Reader.new("inputs")
+    process_files(files)
+  end
+
+  def process_files(files)
+    files.each do |file|
+      process_products(file)
+    end
+  end
+
+  def process_products(file)
+    @order = Order.new
+    file.each do |line|
+      product = Product.new(line)
+      populated_product = ProductSetter.perform(product)
+      taxed_product = SalesTax::Context.apply_tax(populated_product)
+      @order.add(taxed_product)
+    end
+    generate(@order)
+  end
+
+  def generate(order)
+    receipt = Receipt.new(order)
+    receipt.generate_csv
+  end
+end
